24657b22b19b06e1c89854843d99c1559fd75f39
AFIF ASAD RUDDRAW <afifasad1117@gmail.com>
2024-08-01 21:48:08 +0600

    added the API and made the final dataset

---
+++ script.R
@@ -0,0 +1,17 @@
+#load required packages
+library(readr)
+library(kaggler)
+library(dplyr)
+
+#set the API
+response <- kgl_datasets_download_all(owner_dataset = "ashley93/openpowerlifting")
+download.file(response[["url"]], "data/temp.zip", mode="wb")
+unzip_result <- unzip("data/temp.zip", exdir = "data/", overwrite = TRUE)
+powerlift_data <- read.csv("data/openpowerlifting.csv")
+
+colnames(powerlift_data)
+
+final_df <- powerlift_data %>% 
+  select(Name, Sex, Age, Division, BodyweightKg, WeightClassKg, BestSquatKg, BestBenchKg, BestDeadliftKg, TotalKg, Place)
+
+View(final_df)
